098dde924c0ba90b011c05920452b00517bd01b9
blaider <blaider@qq.com>
2014-11-30 22:42:14 +0800

    add mknode_ko

---
+++ mknode_ko/mknode.c
@@ -0,0 +1,81 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  mknode.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年11月30日 22时37分29秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:   (), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+ 
+int HELLO_MAJOR = 0;
+int HELLO_MINOR = 0;
+int NUMBER_OF_DEVICES = 2;
+ 
+struct class *my_class;
+struct cdev cdev;
+dev_t devno;
+ 
+struct file_operations hello_fops = {
+ .owner = THIS_MODULE
+};
+ 
+static int __init hello_init (void)
+{
+    int result;
+    devno = MKDEV(HELLO_MAJOR, HELLO_MINOR);
+    if (HELLO_MAJOR)
+        result = register_chrdev_region(devno, 2, "memdev");
+    else
+    {
+        result = alloc_chrdev_region(&devno, 0, 2, "memdev");
+        HELLO_MAJOR = MAJOR(devno);
+    }  
+    printk("MAJOR IS %d\n",HELLO_MAJOR);
+    my_class = class_create(THIS_MODULE,"hello_char_class");  //类名为hello_char_class
+    if(IS_ERR(my_class)) 
+    {
+        printk("Err: failed in creating class.\n");
+        return -1; 
+    }
+    device_create(my_class,NULL,devno,NULL,"memdev");      //设备名为memdev
+    if (result<0) 
+    {
+        printk (KERN_WARNING "hello: can't get major number %d\n", HELLO_MAJOR);
+        return result;
+    }
+ 
+    cdev_init(&cdev, &hello_fops);
+    cdev.owner = THIS_MODULE;
+    cdev_add(&cdev, devno, NUMBER_OF_DEVICES);
+    printk (KERN_INFO "Character driver Registered\n");
+    return 0;
+}
+ 
+static void __exit hello_exit (void)
+{
+    cdev_del (&cdev);
+    device_destroy(my_class, devno);         //delete device node under /dev//必须先删除设备，再删除class类
+    class_destroy(my_class);                 //delete class created by us
+    unregister_chrdev_region (devno,NUMBER_OF_DEVICES);
+    printk (KERN_INFO "char driver cleaned up\n");
+}
+ 
+module_init (hello_init);
+module_exit (hello_exit);
+ 
+MODULE_LICENSE ("GPL");
