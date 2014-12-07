070ef74f18aaed55659fb3582551046142b57d53
blaider <blaider@qq.com>
2014-12-07 22:53:37 +0800

    add sotest

---
+++ linuxapp/sotest_linux/ta.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ta.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年12月07日 21时16分30秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:   (), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+extern int add(int ,int);
+int main(int argc,char *argv[])
+{
+	add(1,2);
+	return 0;
+}
